08c71ad48f52c57ce39deb6295381e4285cd7031
Aaron Schlitt <aaron.schlitt@student.hpi.uni-potsdam.de>
2024-01-02 16:23:37 +0100

    Implement a logger and document Docker deployment config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import { app } from "./app";
+import { DataLogger } from "./datalogger/datalogger";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  const LOGGING_INTERVAL_HOURS = process.env.LOGGING_INTERVAL_HOURS;
+  const LOGGING_PATH = process.env.LOGGING_PATH;
+  if (LOGGING_INTERVAL_HOURS !== undefined) {
+    if (LOGGING_PATH === undefined) {
+      throw new Error(
+        "LOGGING_PATH must be set if LOGGING_INTERVAL_HOURS is set"
+      );
+    }
+    const loggingInterval = parseInt(LOGGING_INTERVAL_HOURS) * 60 * 60 * 1000;
+    const logger = new DataLogger(loggingInterval, LOGGING_PATH);
+    logger.start();
+  }
+});
