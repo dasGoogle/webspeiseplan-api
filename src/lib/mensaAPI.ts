4999252489d030bd8d903edc7c8557dae14dc8d3
Aaron Schlitt <aaron.schlitt@student.hpi.uni-potsdam.de>
2024-06-03 10:20:51 +0200

    Filter out meals that are not available at the mensa

--- src/lib/mensaAPI.ts
+++ src/lib/mensaAPI.ts
@@ -176,6 +176,8 @@ export class MensaApi {
     const meals: Meal[] = [];
     for (const plan of content) {
       const isEvening = plan.speiseplanAdvanced.titel.includes("Abend");
+      // We don't want to put out meals that are only available for ordering to a vending machine
+      if (plan.speiseplanAdvanced.titel.includes("Automat")) continue;
       if (!plan.speiseplanGerichtData) continue;
       for (const meal of plan.speiseplanGerichtData) {
         if (meal.zusatzinformationen.mitarbeiterpreisDecimal2 === 0) {
