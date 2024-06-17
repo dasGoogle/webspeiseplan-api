9c750388b7c2e6960a9427e04996a186cf91cc58
Aaron Schlitt <aaron.schlitt@student.hpi.uni-potsdam.de>
2024-06-17 10:21:47 +0200

    Serve openapi.json via HTTP

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,7 @@ app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
     swaggerUi.generateHTML(await import("../build/swagger.json"))
   );
 });
+
+app.get("/openapi.json", async (_req: ExRequest, res: ExResponse) => {
+  return res.json(await import("../build/swagger.json"));
+});
